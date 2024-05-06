0eacdd654a2d7d2e119bb6739deaf6105e109f3e
AntonLeskiv <antonlm3105@gmail.com>
2024-05-06 14:15:53 +0200

    Creacion endpoint de crear evento
    Este endpoint sirve para crear nuevos eventos

--- tickets/src/main/java/com/eep/tickets/controllers/EventController.java
+++ tickets/src/main/java/com/eep/tickets/controllers/EventController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.eep.tickets.models.Event;
 import com.eep.tickets.services.EventService;
@@ -21,16 +18,22 @@ public class EventController {
 	private EventService eventService;
 
 	// GET METHODS
-	@GetMapping("event")
+	@GetMapping("/event")
 	public ResponseEntity<List<Event>> getAll() {
 		List<Event> events = eventService.getAll();
 		return ResponseEntity.status(HttpStatus.OK).body(events);
 	}
 
-	@GetMapping("event/{id}")
+	@GetMapping("/event/{id}")
 	public ResponseEntity<Event> getById(@PathVariable Long id) {
 		Event event = eventService.getById(id);
 		return ResponseEntity.status(HttpStatus.OK).body(event);
+	}
+
+	@PostMapping("/event")
+	public ResponseEntity<Event> create(@RequestBody Event event) {
+		Event newEvent = eventService.create(event);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newEvent);
 	}
 
 }
