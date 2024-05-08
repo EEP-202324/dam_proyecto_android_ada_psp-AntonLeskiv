cdbd434216b8861bdfa2136c88816942bc1a45ce
AntonLeskiv <antonlm3105@gmail.com>
2024-05-08 14:16:34 +0200

    Formateo de importaciones

--- tickets-android/app/src/main/java/com/android/tickets_android/MainActivity.kt
+++ tickets-android/app/src/main/java/com/android/tickets_android/MainActivity.kt
@@ -3,7 +3,6 @@ package com.android.tickets_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.android.tickets_android.ui.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
