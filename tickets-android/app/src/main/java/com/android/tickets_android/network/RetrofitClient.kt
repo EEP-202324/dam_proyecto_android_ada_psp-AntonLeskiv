daf9c27b0a6a3938ce60e70774a5c39449226bcf
AntonLeskiv <antonlm3105@gmail.com>
2024-05-08 14:15:26 +0200

    Cambio de nombre y modificacion Retroclient

---
+++ tickets-android/app/src/main/java/com/android/tickets_android/network/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.android.tickets_android.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://10.0.2.2:8000/api/v1/"
+
+    val instance: Retrofit
+        get() = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+}
